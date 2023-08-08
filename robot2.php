58f7bd0b409f3c4a7f581f00b82f6b77e2dd6f5a
mohamedalfraidi <138799844+mohamedalfraidi@users.noreply.github.com>
2023-08-08 19:38:58 +0300

    Add files via upload

---
+++ robot2.php
@@ -0,0 +1,37 @@
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "robot";
+
+
+    $speech=$_POST["voice"];
+
+
+    // Create a new database connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Check the database connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+   
+    $sql = "INSERT INTO robot (speech) move ('$speech')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo $speech;
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    // Close the database connection
+    mysqli_close($conn);
+
+?>
+<html>
+<body>
+<form action="voice.html" method="post">
+    <button type="submit" name="back" value="back">Record new voice</button>
+</form>
+</body>
+</html>
